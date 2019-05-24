d6483922fa23de9bd105b72f12ab2a9b56fe9592
Daniel Aleksandersen <code@daniel.priv.no>
2019-05-25 00:34:47 +0200

    Set HTTP `Cache-Control: max-age=0` response header
    Prevents browsers from caching stylesheets, scripts, and other assets.
    
    Resolves issue #17.

---
+++ adsf/lib/adsf/rack/caching.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Adsf::Rack
+  class Caching
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      status, headers, body = *@app.call(env)
+
+      new_headers =
+        headers.merge(
+          'Cache-Control' => 'max-age=0, stale-while-error=0',
+        )
+
+      [status, new_headers, body]
+    end
+  end
+end
