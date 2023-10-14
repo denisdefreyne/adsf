fe121136dfafc67b0880ec2b406a1336a6bab29f
Denis Defreyne <git@denisdefreyne.com>
2023-10-14 21:48:36 +0200

    Set version to 1.4.8

--- adsf-live/lib/adsf/live/version.rb
+++ adsf-live/lib/adsf/live/version.rb
@@ -2,6 +2,6 @@
 
 module Adsf
   module Live
-    VERSION = '1.4.7'
+    VERSION = '1.4.8'
   end
 end
--- adsf/lib/adsf/version.rb
+++ adsf/lib/adsf/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Adsf
-  VERSION = '1.4.7'
+  VERSION = '1.4.8'
 end
