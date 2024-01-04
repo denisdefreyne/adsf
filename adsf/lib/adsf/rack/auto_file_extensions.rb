5efb09789f445e08a0d1618fe078334e201e9f00
Paul Cantrell <cantrell@pobox.com>
2024-01-04 00:20:57 -0600

    Add auto extensions

---
+++ adsf/lib/adsf/rack/auto_file_extensions.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Adsf::Rack
+  class AutoFileExtensions
+    def initialize(app, root:, extensions: ['.html', '.htm'])
+      @app = app
+      @root = root
+      # Search list starts with '' so that we first look for file as requested
+      @extensions = [''] + Array(extensions)
+    end
+
+    def call(env)
+      path_info = ::Rack::Utils.unescape(env['PATH_INFO'])
+      path = ::File.join(@root, path_info)
+
+      new_env = env
+      @extensions.each do |ext|
+        new_path = path + ext
+        if ::File.exist?(new_path)
+          new_env = env.dup  # only dup if needed
+          new_env['PATH_INFO'] += ext
+          break
+        end
+      end
+
+      @app.call(new_env)
+    end
+  end
+end
